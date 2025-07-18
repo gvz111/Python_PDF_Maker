7ef4d65ca9011b06390fbca65541c23c09db5a9f
gvz111 <robocop220692@yahoo.com>
2025-07-18 12:49:42 +0400

    Added multiple pages for each topic

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(0,0,254)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(10,21, 200,21)#(x1,y1, x2,y2)
+    pdf.line(10,21, 200,21) #(x1,y1, x2,y2)
+    
+    for i in range(row["Pages"] - 1):
+        pdf.add_page()
     
 pdf.output("output.pdf")
