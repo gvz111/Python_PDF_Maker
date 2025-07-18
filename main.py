21fbc4e168eb357fcbcbcac7034c9eaab6ab9734
gvz111 <robocop220692@yahoo.com>
2025-07-18 14:11:16 +0400

    exercise: Added Lines

--- main.py
+++ main.py
@@ -13,14 +13,16 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(0,0,254)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(10,21, 200,21) #(x1,y1, x2,y2)
-    
+        #Set lines
+    for i in range(20,290,10):
+        pdf.line(10,i, 200,i) #(x1,y1, x2,y2)
+        
         #Set the footer
     pdf.ln(260) #added 261 mm break-lines
     pdf.set_font(family="Times", style="I", size=12)
     pdf.set_text_color(180,180,180)
     pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
-    
+        
     for i in range(row["Pages"] - 1):
         pdf.add_page()
         
@@ -29,5 +31,8 @@ for index, row in df.iterrows():
         pdf.set_font(family="Times", style="I", size=12)
         pdf.set_text_color(180,180,180)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+        #Set lines
+        for i in range(20,290,10):
+            pdf.line(10,i, 200,i) #(x1,y1, x2,y2)
     
 pdf.output("output.pdf")
